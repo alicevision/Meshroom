29f97e989e3379d92ef571d4de13f9a7ddbc7662
Cyril Pichard <pih@mikrosimage.eu>
2015-08-31 18:45:35 +0200

    adds missing files from previous commit

---
+++ src/gl/GLDrawable.cpp
@@ -0,0 +1,39 @@
+#include "GLDrawable.hpp"
+
+namespace mockup 
+{
+
+GLSLPlainColorShader* GLDrawable::_colorUniform(nullptr);
+GLSLColoredShader*  GLDrawable::_colorArray(nullptr);
+
+void GLDrawable::setShaders(GLSLPlainColorShader *colorUniform, GLSLColoredShader *colorArray)
+{
+    _colorUniform = colorUniform;
+    _colorArray = colorArray;
+}
+
+void GLDrawable::deleteShaders()
+{
+    if(_colorUniform)
+    {
+        delete _colorUniform;
+        _colorUniform = nullptr;
+    }
+
+    if(_colorArray)
+    {
+        delete _colorArray;
+        _colorArray = nullptr;
+    }
+}
+
+void GLDrawable::setWorldMatrix(const QMatrix4x4 &mat)
+{
+    if(_colorUniform)
+        _colorUniform->setWorldMatrix(mat);
+    if(_colorArray)
+        _colorArray->setWorldMatrix(mat); 
+}
+
+}
+
---
+++ src/gl/GLScene.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <QList>
+#include "GLDrawable.hpp"
+namespace mockup 
+{
+typedef QList<GLDrawable*> GLScene;
+}

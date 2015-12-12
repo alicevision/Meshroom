83c61839acbf98f8c80590f0bf19ad4e230b9ac1
Nicolas Rondaud <nrondaud@gmail.com>
2015-12-12 20:24:16 +0100

    Create the main directory

--- src/models/Job.cpp
+++ src/models/Job.cpp
@@ -239,7 +239,13 @@ bool Job::save()
     // build the JSON object for this job
     QJsonObject json;
     serializeToJSON(&json);
-
+    // create the entire filesystem structure
+    QDir dir;
+    if(!dir.mkpath(_url.toLocalFile()))
+    {
+        qCritical() << _name << ": unable to create the job directory structure";
+        return false;
+    }
     // open a file handler
     QDir jobDirectory(_url.toLocalFile());
     QFile jobFile(jobDirectory.filePath("job.json"));

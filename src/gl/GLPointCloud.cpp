7831a421ecafb396bc2d0c41e17461a3833f921d
Simone Gasparini <simone.gasparini@gmail.com>
2016-08-03 19:44:51 +0200

    [gl] added visibility in Point cloud

--- src/gl/GLPointCloud.cpp
+++ src/gl/GLPointCloud.cpp
@@ -1,6 +1,13 @@
 #include "GLPointCloud.hpp"
 #include "io/AlembicImport.hpp"
+
+#include <QColor>
+
+#include <gl.h>
+
 #include <iostream>
+#include <algorithm>
+#include <iterator>
 
 namespace meshroom
 {
@@ -9,7 +16,11 @@ GLPointCloud::GLPointCloud()
     : GLDrawable(*_colorArray)
     , _pointPositions(QOpenGLBuffer::VertexBuffer)
     , _pointColors(QOpenGLBuffer::VertexBuffer)
+    , _pointColorsVisibility(QOpenGLBuffer::VertexBuffer)
+    , _pointVisibility()
     , _npoints(0)
+    , _visibilityThreshold(0) 
+    , _maxVisibility(5)
 {
     _vertexArrayObject.create();
 }
@@ -31,6 +42,32 @@ void GLPointCloud::setRawPositions(const void* pointsBuffer, size_t npoints)
         _program.enableAttributeArray("in_position");
         _program.setAttributeBuffer("in_position", GL_FLOAT, 0, 3);
         _pointPositions.release();
+        
+        {
+          //@todo remove me just fake
+          _pointVisibility.resize(npoints, 0);
+          for(std::size_t i = 0; i < npoints; ++i)
+          {
+              _pointVisibility[i] = i % 100;
+              if(_pointVisibility[i] > _maxVisibility)
+                  _maxVisibility = _pointVisibility[i];
+          }
+          std::cout << "max visibility = " << _maxVisibility << std::endl;
+          if(_pointColorsVisibility.create())
+          {
+              std::vector<float> colors(npoints * 3, 1.0);
+              _pointColorsVisibility.setUsagePattern(QOpenGLBuffer::StaticDraw);
+              _pointColorsVisibility.bind();
+              _pointColorsVisibility.allocate(colors.data(), npoints * 3 * sizeof(float));
+              _program.enableAttributeArray("in_color");
+              _program.setAttributeBuffer("in_color", GL_FLOAT, 0, 3);
+              _pointColorsVisibility.release();
+          }
+          else
+          {
+              std::cout << "unable to create buffer for point cloud visibility" << std::endl;
+          }
+        }
     }
     else
     {
@@ -58,6 +95,49 @@ void GLPointCloud::setRawColors(const void* pointsBuffer, size_t npoints)
     _vertexArrayObject.release();
 }
 
+void GLPointCloud::setRawVisibility(const std::vector<std::size_t>& vec_visibility, 
+                                    std::size_t npoints)
+{
+    //@todo copy points into _pointVisibility
+    
+    if(npoints == 0)
+    {
+        std::cout << "[GLPointCloud::setRawVisibility] no points!" << std::endl;
+        return;
+    }
+    
+
+    //@todo set _maxVisibility
+
+//    _pointVisibility.resize(npoints, 0);
+//    for(std::size_t i = 0; i < npoints; ++i)
+//    {
+//        _pointVisibility[i] = i % 100;
+//        if(_pointVisibility[i] > _maxVisibility)
+//            _maxVisibility = _pointVisibility[i];
+//    }
+    _pointVisibility = vec_visibility;
+    
+    _maxVisibility = *std::max_element(_pointVisibility.begin(), _pointVisibility.end());
+    std::cout << "max visibility = " << _maxVisibility << std::endl;
+    
+    if(_pointColorsVisibility.create())
+    {
+        std::vector<float> colors(npoints * 3, 1.0);
+        _pointColorsVisibility.setUsagePattern(QOpenGLBuffer::StaticDraw);
+        _pointColorsVisibility.bind();
+        _pointColorsVisibility.allocate(colors.data(), npoints * 3 * sizeof (float));
+        _program.enableAttributeArray("in_color");
+        _program.setAttributeBuffer("in_color", GL_FLOAT, 0, 3);
+        _pointColorsVisibility.release();
+    }
+    else
+    {
+        std::cout << "unable to create buffer for point cloud visibility" << std::endl;
+    }
+
+}
+
 void GLPointCloud::draw()
 {
     if(_npoints)
@@ -67,6 +147,66 @@ void GLPointCloud::draw()
         glDrawArrays(GL_POINTS, 0, _npoints);
         _vertexArrayObject.release();
         _program.release();
+    }
+}
+
+
+void GLPointCloud::setVisibilityThreshold(float threshold)
+{
+    _visibilityThreshold = (std::size_t) (threshold * _maxVisibility);
+    std::cout << "GLPointCloud::_visibilityThreshold = " << _visibilityThreshold << std::endl;
+    //@todo recompute colors accordingly
+    
+    if(_visibilityThreshold > 2)
+    {
+      std::vector<float> colors(_npoints * 3, 1.0);
+      // for each point
+      for(std::size_t i = 0; i < _npoints; ++i)
+      {
+        // if visibility < _visibilityThreshold
+          if(_pointVisibility[i] < _visibilityThreshold)
+          {
+              // set its color to some grey level
+              colors[i*3] = 0.3;
+              colors[i*3+1] = 0.3;
+              colors[i*3+2] = 0.3;
+          }
+          else
+          {
+              assert(_maxVisibility > 0);
+              const float normalized = _pointVisibility[i] / float(_maxVisibility);
+              // set the color using normalized visibility  (visibility/_maxVisibility)
+              // use a HVS ramp HVS(normVisibility, 1.0 ,1.0)
+              QColor color;
+              color.setHsv((int)(255*normalized), 255, 255);
+              colors[i*3] = (float) color.redF();
+              colors[i*3+1] = (float) color.greenF();
+              colors[i*3+2] = (float) color.blueF();
+//              std::cout << "colors[i*3] " << colors[i*3] << "colors[i*3+1] " << colors[i*3+1] << "colors[i*3+2] " << colors[i*3+2] << std::endl;
+          }
+      }
+
+      glEnable(GL_PROGRAM_POINT_SIZE);
+      glPointSize(3.0);
+      _vertexArrayObject.bind();
+      _pointColorsVisibility.bind();
+      _pointColorsVisibility.write(0, colors.data(), _npoints * 3 * sizeof (float));
+      _program.enableAttributeArray("in_color");
+      _program.setAttributeBuffer("in_color", GL_FLOAT, 0, 3);
+      _pointColorsVisibility.release();
+      _vertexArrayObject.release();
+    }
+    else
+    {
+        glDisable(GL_PROGRAM_POINT_SIZE);
+        glPointSize(1.0);
+        // use normal colors
+        _vertexArrayObject.bind();
+        _pointColors.bind();
+        _program.enableAttributeArray("in_color");
+        _program.setAttributeBuffer("in_color", GL_FLOAT, 0, 3);
+        _pointColors.release();
+        _vertexArrayObject.release();
     }
 }
 
--- src/gl/GLPointCloud.hpp
+++ src/gl/GLPointCloud.hpp
@@ -15,14 +15,21 @@ public:
     ~GLPointCloud() = default;
 
     void draw() override;
-    void setRawPositions(const void* points, size_t npoints);
-    void setRawColors(const void* points, size_t npoints);
+    void setRawPositions(const void* points, std::size_t npoints);
+    void setRawColors(const void* points, std::size_t npoints);
+    void setRawVisibility(const std::vector<std::size_t>& vec_visibility, std::size_t npoints);
+    void setVisibilityThreshold(float threshold); 
+    inline std::size_t getVisibility() { return _visibilityThreshold; }
 
 private:
     QOpenGLVertexArrayObject _vertexArrayObject;
     QOpenGLBuffer _pointPositions;
     QOpenGLBuffer _pointColors;
+    QOpenGLBuffer _pointColorsVisibility;
+    std::vector<std::size_t> _pointVisibility;
     GLint _npoints;
+    std::size_t _visibilityThreshold;
+    std::size_t _maxVisibility;
 };
 
 } // namespace

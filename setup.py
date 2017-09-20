82701e65552e5f618bbaf0ceaf9e1d3cc5e74404
Yann Lanthony <yann.lanthony@gmail.com>
2017-09-20 17:59:07 +0200

    [setup.py] remove extra 'pytest'

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="Meshroom",
     description="Photogrammetry Software",
-    requires=['psutil', 'pytest', 'pySide2', "pytest"]
+    requires=['psutil', 'pytest', 'pySide2']
 )

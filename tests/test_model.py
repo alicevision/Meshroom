4cd9627263e2938f304f7b8c4621af5eb7137f20
Yann Lanthony <yann.lanthony@gmail.com>
2017-09-25 18:53:22 +0200

    [tests] add basic tests for data models

---
+++ tests/test_model.py
@@ -0,0 +1,53 @@
+import pytest
+
+from PySide2.QtCore import QObject, Property
+
+from meshroom.types.core import CoreModel
+from meshroom.types.qt import QObjectListModel, QTypedObjectListModel
+
+
+class DummyNode(QObject):
+
+    def __init__(self, name="", parent=None):
+        super(DummyNode, self).__init__(parent)
+        self._name = name
+
+    def getName(self):
+        return self._name
+
+    name = Property(str, getName)
+
+
+def test_coreModel_add_remove():
+    m = CoreModel()
+    node = DummyNode("DummyNode_1")
+    m.add(node)
+    assert len(m) == 1
+    assert m.get("DummyNode_1") == node
+
+    m.pop("DummyNode_1")
+    assert len(m) == 0
+
+
+def test_listModel_add_remove():
+    m = QObjectListModel()
+    node = DummyNode("DummyNode_1")
+    m.add(node)
+    assert len(m) == 1
+    assert m.get("DummyNode_1") == node
+
+    m.remove(node)
+    assert len(m) == 0
+
+
+def test_listModel_typed_add():
+    m = QTypedObjectListModel(T=DummyNode)
+    assert m.roleForName('name') != -1
+
+    node = DummyNode("DummyNode_1")
+    m.add(node)
+    assert m.data(m.index(0), m.roleForName('name')) == "DummyNode_1"
+
+    obj = QObject()
+    with pytest.raises(TypeError):
+        m.add(obj)

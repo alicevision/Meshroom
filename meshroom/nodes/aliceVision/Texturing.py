eaa22528f16c867fdf471848970755e06229ad20
Fabien Castan <faca@mikrosimage.eu>
2017-10-31 16:47:34 +0100

    [nodes] expose texturing parameters

--- meshroom/nodes/aliceVision/Texturing.py
+++ meshroom/nodes/aliceVision/Texturing.py
@@ -12,4 +12,50 @@ class Texturing(desc.CommandLineNode):
             value='',
             uid=[0],
             isOutput=False,
+            group='',
             )
+
+    output = desc.File(
+            label='Output Folder',
+            description='Folder for output mesh: OBJ, material and texture files.',
+            value='{cache}/{nodeType}/{uid0}/',
+            uid=[0],
+            isOutput=True,
+            )
+
+    inputMesh = desc.File(
+            label='Other Input Mesh',
+            description='Optional input mesh to texture. By default, it will texture the result of the reconstruction.',
+            value='',
+            uid=[0],
+            isOutput=False,
+            )
+
+    textureSide = desc.IntParam(
+            label='Texture Side',
+            description='''Output texture size''',
+            value=8192,
+            range=(1024, 16384, 1024),
+            uid=[0],
+            )
+    padding = desc.IntParam(
+            label='Padding',
+            description='''Texture edge padding size in pixel''',
+            value=15,
+            range=(0, 100, 1),
+            uid=[0],
+            )
+    downscale = desc.IntParam(
+            label='Downscale',
+            description='''Texture downscale factor''',
+            value=2,
+            range=(0, 16, 1),
+            uid=[0],
+            )
+    flipNormals = desc.BoolParam(
+            label='Flip Normals',
+            description='''Option to flip face normals. It can be needed as it depends on the vertices order in triangles and the convention change from one software to another.''',
+            value=False,
+            uid=[0],
+            )
+

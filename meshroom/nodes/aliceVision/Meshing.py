96ac1216c3a303c1578f376538236514f7328c5a
Fabien Castan <faca@mikrosimage.eu>
2017-10-31 21:19:07 +0100

    [nodes] meshing: fix command line

--- meshroom/nodes/aliceVision/Meshing.py
+++ meshroom/nodes/aliceVision/Meshing.py
@@ -2,7 +2,7 @@ from meshroom.core import desc
 
 class Meshing(desc.CommandLineNode):
     internalFolder = '{cache}/{nodeType}/{uid0}/'
-    commandLine = 'CMPMVS {mvsConfigValue} --meshing'
+    commandLine = 'aliceVision_meshing {allParams}'
     cpu = desc.Level.INTENSIVE
     ram = desc.Level.INTENSIVE
 
@@ -13,5 +13,15 @@ class Meshing(desc.CommandLineNode):
             description='',
             value='',
             uid=[0],
+            ),
+    ]
+
+    outputs = [
+        desc.File(
+            name="output",
+            label="Output mesh",
+            description="Output mesh (OBJ file format).",
+            value='{cache}/{nodeType}/{uid0}/mesh.obj',
+            uid=[],
             ),
     ]

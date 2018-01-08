7629236731169bc894eed38f25610e45f3ad8dfc
Fabien Castan <faca@mikrosimage.eu>
2018-01-08 21:50:14 +0100

    [nodes] CameraInit: fix quotes

--- meshroom/nodes/aliceVision/CameraInit.py
+++ meshroom/nodes/aliceVision/CameraInit.py
@@ -123,7 +123,7 @@ class CameraInit(desc.CommandLineNode):
                 logging.warning('CameraInit: Error on buildIntrinsics of node "{}".'.format(node.name))
 
             # Reload result of aliceVision_cameraInit
-            cameraInitSfM = localCmdVars['outputValue']
+            cameraInitSfM = node.output.value
             jsonData = open(cameraInitSfM, 'r').read()
             data = json.loads(jsonData)
 

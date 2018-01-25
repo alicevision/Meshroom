b8799bb9f05c11a74118fbc0dbbeabd40836a8eb
Gregoire De Lillo <gregoire.delillo@me.com>
2018-01-25 17:09:20 +0100

    [nodes] new node `SfMAlignment`

---
+++ meshroom/nodes/aliceVision/SfMAlignment.py
@@ -0,0 +1,45 @@
+import sys
+import os
+from meshroom.core import desc
+
+
+class SfMAlignment(desc.CommandLineNode):
+    internalFolder = '{cache}/{nodeType}/{uid0}/'
+    commandLine = 'aliceVision_utils_sfmAlignment {allParams}'
+    size = desc.DynamicNodeSize('input')
+
+    inputs = [
+        desc.File(
+            name='input',
+            label='Input',
+            description='''SfMData file .''',
+            value='',
+            uid=[0],
+        ),
+        desc.File(
+            name='reference',
+            label='Reference',
+            description='''Path to the scene used as the reference coordinate system.''',
+            value='',
+            uid=[0],
+        ),
+        desc.ChoiceParam(
+            name='verboseLevel',
+            label='Verbose Level',
+            description='''verbosity level (fatal, error, warning, info, debug, trace).''',
+            value='info',
+            values=['fatal', 'error', 'warning', 'info', 'debug', 'trace'],
+            exclusive=True,
+            uid=[],
+        ),
+    ]
+
+    outputs = [
+        desc.File(
+            name='output',
+            label='Output',
+            description='''Aligned SfMData file .''',
+            value='{cache}/{nodeType}/{uid0}/alignedSfM.abc',
+            uid=[],
+        ),
+    ]

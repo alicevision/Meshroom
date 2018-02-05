d5f2ac76bcda82c4edae37502ee5e75d498ed6c5
Yann Lanthony <yann.lanthony@gmail.com>
2018-02-05 19:18:12 +0100

    [nodes] FeatureMatching: improve description layout

--- meshroom/nodes/aliceVision/FeatureMatching.py
+++ meshroom/nodes/aliceVision/FeatureMatching.py
@@ -54,7 +54,13 @@ class FeatureMatching(desc.CommandLineNode):
         desc.ChoiceParam(
             name='photometricMatchingMethod',
             label='Photometric Matching Method',
-            description='''For Scalar based regions descriptor: * BRUTE_FORCE_L2: L2 BruteForce matching * ANN_L2: L2 Approximate Nearest Neighbor matching * CASCADE_HASHING_L2: L2 Cascade Hashing matching * FAST_CASCADE_HASHING_L2: L2 Cascade Hashing with precomputed hashed regions (faster than CASCADE_HASHING_L2 but use more memory) For Binary based descriptor: * BRUTE_FORCE_HAMMING: BruteForce Hamming matching''',
+            description='For Scalar based regions descriptor\n'
+                        ' * BRUTE_FORCE_L2: L2 BruteForce matching\n'
+                        ' * ANN_L2: L2 Approximate Nearest Neighbor matching\n'
+                        ' * CASCADE_HASHING_L2: L2 Cascade Hashing matching\n'
+                        ' * FAST_CASCADE_HASHING_L2: L2 Cascade Hashing with precomputed hashed regions (faster than CASCADE_HASHING_L2 but use more memory) \n'
+                        'For Binary based descriptor\n'
+                        ' * BRUTE_FORCE_HAMMING: BruteForce Hamming matching',
             value='ANN_L2',
             values=('BRUTE_FORCE_L2', 'ANN_L2', 'CASCADE_HASHING_L2', 'FAST_CASCADE_HASHING_L2', 'BRUTE_FORCE_HAMMING'),
             exclusive=True,

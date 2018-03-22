c700d4c071e8b894f31f44d124d4dcf95de57011
Fabien Castan <faca@mikrosimage.eu>
2018-03-22 17:34:38 +0100

    [nodes] verbose param on more Mesh processing nodes

--- meshroom/nodes/aliceVision/MeshFiltering.py
+++ meshroom/nodes/aliceVision/MeshFiltering.py
@@ -30,6 +30,15 @@ class MeshFiltering(desc.CommandLineNode):
             range=(0.0, 10.0, 0.1),
             uid=[0],
             ),
+        desc.ChoiceParam(
+            name='verboseLevel',
+            label='Verbose Level',
+            description='''verbosity level (fatal, error, warning, info, debug, trace).''',
+            value='info',
+            values=['fatal', 'error', 'warning', 'info', 'debug', 'trace'],
+            exclusive=True,
+            uid=[],
+        ),
     ]
 
     outputs = [

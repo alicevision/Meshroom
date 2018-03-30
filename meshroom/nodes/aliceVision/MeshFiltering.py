b4acee2ee101051ce2084b7e4bc1e84808aa386c
Fabien Castan <faca@mikrosimage.eu>
2018-03-30 22:02:07 +0200

    [nodes] MeshFiltering: add options to remove triangles
    - add option to remove large triangles
    - add option to keep only the largest group of connected triangles

--- meshroom/nodes/aliceVision/MeshFiltering.py
+++ meshroom/nodes/aliceVision/MeshFiltering.py
@@ -14,6 +14,21 @@ class MeshFiltering(desc.CommandLineNode):
             value='',
             uid=[0],
             ),
+        desc.FloatParam(
+            name='removeLargeTrianglesFactor',
+            label='Filter Large Triangles Factor',
+            description='Remove all large triangles. We consider a triangle as large if one edge is bigger than N times the average edge length. Put zero to disable it.',
+            value=60.0,
+            range=(1.0, 100.0, 0.1),
+            uid=[0],
+            ),
+        desc.BoolParam(
+            name='keepLargestMeshOnly',
+            label='Keep Only the Largest Mesh',
+            description='Keep only the largest connected triangles group.',
+            value=True,
+            uid=[0],
+            ),
         desc.IntParam(
             name='iterations',
             label='Nb Iterations',

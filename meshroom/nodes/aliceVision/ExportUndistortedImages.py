51248d72f87c7e95f634dd1348c44523fd1735bc
Grégoire De Lillo <gregoire-dl@users.noreply.github.com>
2018-06-06 12:05:35 +0200

    [nodes] Add node `ExportUndistortedImages`

---
+++ meshroom/nodes/aliceVision/ExportUndistortedImages.py
@@ -0,0 +1,43 @@
+from meshroom.core import desc
+
+class ExportUndistortedImages(desc.CommandLineNode):
+    internalFolder = '{cache}/{nodeType}/{uid0}/'
+    commandLine = 'aliceVision_exportUndistortedImages {allParams}'
+
+    inputs = [
+        desc.File(
+            name='input',
+            label='Input SfMData',
+            description='SfMData file containing a complete SfM.',
+            value='',
+            uid=[0],
+        ),
+        desc.ChoiceParam(
+            name='outputFileType',
+            label='Output File Type',
+            description='Output file type for the undistorted images.',
+            value='exr',
+            values=['jpg', 'png', 'tif', 'exr'],
+            exclusive=True,
+            uid=[0],
+        ),
+        desc.ChoiceParam(
+            name='verboseLevel',
+            label='Verbose Level',
+            description='Verbosity level (fatal, error, warning, info, debug, trace).',
+            value='info',
+            values=['fatal', 'error', 'warning', 'info', 'debug', 'trace'],
+            exclusive=True,
+            uid=[],
+        )
+    ]
+
+    outputs = [
+        desc.File(
+            name='output',
+            label='Output Folder',
+            description='Output folder for the undistorted images.',
+            value='{cache}/{nodeType}/{uid0}/',
+            uid=[],
+        ),
+    ]

6a138e492ae45164ab3d5566512b02c6072caf88
Fabien Castan <fabcastan@gmail.com>
2018-07-23 18:08:36 +0200

    [nodes] CameraInit: use json to load the metadata string

--- meshroom/nodes/aliceVision/CameraInit.py
+++ meshroom/nodes/aliceVision/CameraInit.py
@@ -170,14 +170,10 @@ class CameraInit(desc.CommandLineNode):
                 intrinsic['principalPoint'] = [intrinsic['principalPoint']['x'], intrinsic['principalPoint']['y']]
             views = node.viewpoints.getPrimitiveValue(exportDefault=False)
 
-            # convert metadata string into a map
+            # convert the metadata string into a map
             for view in views:
-                # filter out unnecessary attributes
                 if 'metadata' in view:
-                    try:
-                        view['metadata'] = eval(view['metadata'])
-                    except:
-                        del view['metadata']
+                    view['metadata'] = json.loads(view['metadata'])
 
             sfmData = {
                 "version": [1, 0, 0],

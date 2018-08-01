b8730324c7afe547d1f5ffed98ce4b6f16a3eb6c
Yann Lanthony <yann.lanthony@gmail.com>
2018-08-01 19:40:37 +0200

    [setup] add 'setupEnvironment' for setting-up standalone configuration
    * setup environment vars to work in a prebuilt, standalone configuration
    * call 'setupEnvironment' at the beginning of executables

--- meshroom/ui/__main__.py
+++ meshroom/ui/__main__.py
@@ -1,10 +1,12 @@
 import signal
 import sys
-
-from meshroom.ui.app import MeshroomApp
+import meshroom
 
 
 if __name__ == "__main__":
+    meshroom.setupEnvironment()
+
     signal.signal(signal.SIGINT, signal.SIG_DFL)
+    from meshroom.ui.app import MeshroomApp
     app = MeshroomApp(sys.argv)
     app.exec_()

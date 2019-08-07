6d377e1b7d07aa6dec7ccda20684ec8ce717d07c
Anouk Liberge <anouk.liberge@orange.fr>
2019-08-07 15:19:43 +0200

    [nodes][aliceVision] hdr: change descriptions and label parameters

--- meshroom/nodes/aliceVision/LDRToHDR.py
+++ meshroom/nodes/aliceVision/LDRToHDR.py
@@ -12,9 +12,9 @@ class LDRToHDR(desc.CommandLineNode):
                 name='inputFolder',
                 label='Input File/Folder',
                 description="Folder containing LDR images",
-            value='',
-            uid=[0],
-            ),
+                value='',
+                uid=[0],
+                ),
             name="input",
             label="Input Files or Folders",
             description='Folders containing LDR images.',
@@ -33,62 +33,62 @@ class LDRToHDR(desc.CommandLineNode):
                         " * linear \n"
                         " * robertson \n"
                         " * debevec \n"
-                        " * beta: grossberg",
+                        " * grossberg",
             values=['linear', 'robertson', 'debevec', 'grossberg'],
             value='linear',
             exclusive=True,
             uid=[0],
-            ),
+        ),
         desc.File(
             name='inputResponse',
             label='Input Response',
             description="external camera response file path to fuse all LDR images together.",
             value='',
             uid=[0],
-            ),
+        ),
         desc.StringParam(
             name='targetExposureImage',
             label='Target Exposure Image',
-            description="LDR image at the target exposure for the output HDR image to be centered.",
+            description="LDR image(s) name(s) at the target exposure for the output HDR image(s) to be centered.",
             value='',
             uid=[0],
-            ),
+        ),
         desc.ChoiceParam(
             name='calibrationWeight',
             label='Calibration Weight',
-            description="Weight function type (default, gaussian, triangle, plateau).",
+            description="Weight function used to calibrate camera response \n"
+                        " * default \n"
+                        " * gaussian \n"
+                        " * triangle \n""
+                        " * plateau",
             value='default',
             values=['default', 'gaussian', 'triangle', 'plateau'],
             exclusive=True,
             uid=[0],
-            ),
+        ),
         desc.ChoiceParam(
             name='fusionWeight',
             label='Fusion Weight',
-            description="Weight function used to fuse all LDR images together (gaussian, triangle, plateau).",
+            description="Weight function used to fuse all LDR images together \n""
+                        " *  gaussian \n"
+                        " * triangle \n" 
+                        " * plateau",
             value='gaussian',
             values=['gaussian', 'triangle', 'plateau'],
             exclusive=True,
             uid=[0],
-            ),
+        ),
         desc.FloatParam(
-            name='oversaturatedCorrection',
-            label='Oversaturated Correction',
-            description="Oversaturated correction for pixels oversaturated in all images: \n"
+            name='expandDynamicRange',
+            label='Expand Dynamic Range',
+            description="Correction of clamped high values in dynamic range: \n"
                         " - use 0 for no correction \n"
                         " - use 0.5 for interior lighting \n" 
                         " - use 1 for outdoor lighting",
             value=1,
             range=(0, 1, 0.1),
             uid=[0],
-            ),
-        desc.File(
-            name='recoverPath',
-            label='Recover Path',
-            description="Path to write recovered LDR image at the target exposure by applying inverse response on HDR image.",
-            value='',
-            uid=[0],
-            ),
+        ),
         desc.ChoiceParam(
             name='verboseLevel',
             label='Verbose Level',
@@ -97,7 +97,15 @@ class LDRToHDR(desc.CommandLineNode):
             values=['fatal', 'error', 'warning', 'info', 'debug', 'trace'],
             exclusive=True,
             uid=[],
-            ),
+        ),
+        desc.File(
+            name='recoverPath',
+            label='Output Recovered Files',
+            description="(debug) Folder for recovered LDR images at target exposures.",
+            advanced=True,
+            value='',
+            uid=[],
+        ),
     ]
 
     outputs = [
@@ -107,12 +115,12 @@ class LDRToHDR(desc.CommandLineNode):
             description="Output HDR image path.",
             value=desc.Node.internalFolder + 'hdr.exr',
             uid=[],
-            ),
+        ),
         desc.File(
             name='outputResponse',
             label='Output Response',
             description="Output response function path.",
-            value=desc.Node.internalFolder + 'response.ods',
+            value=desc.Node.internalFolder + 'response.csv',
             uid=[],
-            ),
+        ),
     ]

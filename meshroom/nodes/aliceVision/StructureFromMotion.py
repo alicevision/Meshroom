2f6aa70574a24d4b011f3dea6beadd59236783ae
natowi <natowi@users.noreply.github.com>
2019-10-11 10:48:16 +0200

    Update StructureFromMotion.py

--- meshroom/nodes/aliceVision/StructureFromMotion.py
+++ meshroom/nodes/aliceVision/StructureFromMotion.py
@@ -47,7 +47,7 @@ class StructureFromMotion(desc.CommandLineNode):
             label='Describer Types',
             description='Describer types used to describe an image.',
             value=['sift'],
-            values=['akaze', 'akaze_liop', 'akaze_mldb', 'sift', 'sift_float', 'sift_upright', 'akaze_ocv', 'brisk_ocv', 'sift_ocv', 'surf_ocv','cctag3', 'cctag4'],
+            values=['akaze', 'akaze_liop', 'akaze_mldb', 'sift', 'sift_float', 'sift_upright', 'akaze_ocv', 'brisk_ocv', 'sift_ocv', 'surf_ocv', 'cctag3', 'cctag4'],
             exclusive=False,
             uid=[0],
             joinChar=',',

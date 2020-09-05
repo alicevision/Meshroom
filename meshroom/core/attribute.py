635f36216b8175d5181a0419afa93407c7b0eef7
Julien-Haudegond <44610840+Julien-Haudegond@users.noreply.github.com>
2020-09-05 13:05:20 +0200

    [core] Attribute.isLink: fix unreachable return

--- meshroom/core/attribute.py
+++ meshroom/core/attribute.py
@@ -171,9 +171,7 @@ class Attribute(BaseObject):
     def isLink(self):
         """ Whether the attribute is a link to another attribute. """
         # note: directly use self.node.graph._edges to avoid using the property that may become invalid at some point
-        return self.node.graph and self.isInput and self in self.node.graph._edges.keys()
-
-        return self in self.node.graph.edges.keys()
+        return self.node.graph and self.isInput and self.node.graph._edges and self in self.node.graph._edges.keys()
 
     @staticmethod
     def isLinkExpression(value):

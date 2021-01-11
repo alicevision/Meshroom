402c315e6bedba471a413cd6d7112ae360aa609a
ludchieng <lud.chieng@hotmail.fr>
2021-01-11 19:29:34 +0300

    [nodes] ColorCheckerDetection: update params

--- meshroom/nodes/aliceVision/ColorCheckerDetection.py
+++ meshroom/nodes/aliceVision/ColorCheckerDetection.py
@@ -12,8 +12,8 @@ class ColorCheckerDetection(desc.CommandLineNode):
     # commandLineRange = '--rangeStart {rangeStart} --rangeSize {rangeBlockSize}'
 
     documentation = '''
-        TODO
-    '''
+TODO
+'''
 
     inputs = [
         desc.File(
@@ -22,5 +22,22 @@ class ColorCheckerDetection(desc.CommandLineNode):
             description='SfMData file input, image filenames or regex(es) on the image file path.\nsupported regex: \'#\' matches a single digit, \'@\' one or more digits, \'?\' one character and \'*\' zero or more.',
             value='',
             uid=[0],
-        )
+        ),
+        desc.BoolParam(
+            name='debug',
+            label='Debug',
+            description='If checked, debug data will be generated',
+            value=True,
+            uid=[0],
+        ),
+    ]
+
+    outputs = [
+        desc.File(
+            name='output',
+            label='Output Color Checker Data',
+            description='Output colorimetric data extracted from a detected color checker in the images',
+            value=desc.Node.internalFolder,
+            uid=[],
+        ),
     ]

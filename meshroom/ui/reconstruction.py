0ddda9f9822c50a81c613c8366c2d51696d070ce
Fabien Castan <fabcastan@gmail.com>
2021-04-16 10:40:20 +0200

    [multiview] new experimental pipeline for camera tracking

--- meshroom/ui/reconstruction.py
+++ meshroom/ui/reconstruction.py
@@ -490,6 +490,9 @@ class Reconstruction(UIGraph):
         elif p.lower() == "panoramafisheyehdr":
             # default panorama fisheye hdr pipeline
             self.setGraph(multiview.panoramaFisheyeHdr())
+        elif p.lower() == "cameratracking":
+            # default camera tracking pipeline
+            self.setGraph(multiview.cameraTracking())
         else:
             # use the user-provided default photogrammetry project file
             self.load(p, setupProjectFile=False)

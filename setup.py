f21a28b0829eab148aaf03c46c4935d45115dd87
Grégoire De Lillo <gregoire-dl@users.noreply.github.com>
2021-05-04 12:18:15 +0200

    [setup] Update install_requires for PySide 6

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ executables = [
 setup(
     name="Meshroom",
     description="Meshroom",
-    install_requires=['psutil', 'pytest', 'PySide2', 'markdown'],
+    install_requires=['psutil', 'pytest', 'PySide6', 'markdown'],
     extras_require={
         ':python_version < "3.4"': [
             'enum34',

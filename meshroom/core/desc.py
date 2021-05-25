b000c6f3119ce47f761e11245f1c6984e4ed308b
Fabien Castan <fabcastan@gmail.com>
2021-05-25 18:22:08 +0200

    [core] no cmd line range arguments if we have only a single chunk

--- meshroom/core/desc.py
+++ meshroom/core/desc.py
@@ -470,7 +470,7 @@ class CommandLineNode(Node):
             if not alreadyInEnv:
                 cmdPrefix = '{rez} {packageFullName} -- '.format(rez=os.environ.get('REZ_ENV'), packageFullName=chunk.node.packageFullName)
         cmdSuffix = ''
-        if chunk.node.isParallelized:
+        if chunk.node.isParallelized and chunk.node.size > 1:
             cmdSuffix = ' ' + self.commandLineRange.format(**chunk.range.toDict())
         return cmdPrefix + chunk.node.nodeDesc.commandLine.format(**chunk.node._cmdVars) + cmdSuffix
 

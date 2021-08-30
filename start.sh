b1c421bfeffc657bce6fd010f2d73c3218b3be68
Remy Mellet <remy.mellet@gmail.com>
2021-08-30 19:41:52 +0200

    [doc] how to use Meshroom without building AliceVision (#1487)
    * documentation: how to use Meshroom without building AliceVision
    * documentation: explain how to start and debug with PyCharm
    * add comments as examples in start.sh
    * fix wrong bash and if not in right dir (related with https://github.com/alicevision/meshroom/commit/692ecfa74d473d2a0433580d6f761a6aed754317#commitcomment-53971206)

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
-export PYTHONPATH="$(dirname "$(readlink -f "${BASH_SOURCE[0]}" )" )"
-python meshroom/ui
+#!/bin/bash
+export MESHROOM_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}" )" )"
+export PYTHONPATH=$MESHROOM_ROOT:$PYTHONPATH
+
+# using existing alicevision release
+#export LD_LIBRARY_PATH=/foo/Meshroom-2021.1.0/aliceVision/lib/
+#export PATH=$PATH:/foo/Meshroom-2021.1.0/aliceVision/bin/
+
+# using alicevision built source
+#export PATH=$PATH:/foo/build/Linux-x86_64/
+
+python "$MESHROOM_ROOT/meshroom/ui"

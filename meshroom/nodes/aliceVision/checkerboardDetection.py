27d1e200d073038836248b5bfb57868e64d3ef7f
fabien servant <fabien.servant@technicolor.com>
2022-03-09 16:54:36 +0100

    Checkerboard detection node

---
+++ meshroom/nodes/aliceVision/checkerboardDetection.py
@@ -0,0 +1,47 @@
+__version__ = "1.0"
+
+import json
+import os
+
+from meshroom.core import desc
+
+
+class CheckerboardDetection(desc.CommandLineNode):
+    commandLine = 'aliceVision_checkerboardDetection {allParams}'
+    size = desc.DynamicNodeSize('input')
+    parallelization = desc.Parallelization(blockSize=5)
+    commandLineRange = '--rangeStart {rangeStart} --rangeSize {rangeBlockSize}'
+
+    category = 'Other'
+    documentation = '''
+Compute the image warping for each input image in the panorama coordinate system.
+'''
+
+    inputs = [
+        desc.File(
+            name='input',
+            label='Input',
+            description="SfM Data File",
+            value='',
+            uid=[0],
+        ),
+        desc.ChoiceParam(
+            name='verboseLevel',
+            label='Verbose Level',
+            description='Verbosity level (fatal, error, warning, info, debug, trace).',
+            value='info',
+            values=['fatal', 'error', 'warning', 'info', 'debug', 'trace'],
+            exclusive=True,
+            uid=[],
+        ),
+    ]
+
+    outputs = [
+        desc.File(
+            name='output',
+            label='Output File',
+            description='',
+            value=desc.Node.internalFolder,
+            uid=[],
+        ),
+    ]

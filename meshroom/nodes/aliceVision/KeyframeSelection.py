c53d17650f765fd4f644713af25b72bfc5986e67
fabien servant <fabien.servant@technicolor.com>
2022-09-05 11:28:59 +0200

    [node] keyframeselection update with simpler algorithms

--- meshroom/nodes/aliceVision/KeyframeSelection.py
+++ meshroom/nodes/aliceVision/KeyframeSelection.py
@@ -65,84 +65,12 @@ You can extract frames at regular interval by configuring only the min/maxFrameS
             label="mmFocals",
             description="Focals in mm (will be use if not 0)."
         ),
-        desc.ListAttribute(
-            elementDesc=desc.FloatParam(
-                name="pxFocal",
-                label="pxFocal",
-                description="Focal in px (will be use and convert in mm if not 0).",
-                value=0.0,
-                range=(0.0, 500.0, 1.0),
-                uid=[0],
-            ),
-            name="pxFocals",
-            label="pxFocals",
-            description="Focals in px (will be use and convert in mm if not 0)."
-        ),
-        desc.ListAttribute(
-            elementDesc=desc.IntParam(
-                name="frameOffset",
-                label="Frame Offset",
-                description="Frame offset.",
-                value=0,
-                range=(0, 100, 1),
-                uid=[0],
-            ),
-            name="frameOffsets",
-            label="Frame Offsets",
-            description="Frame offsets."
-        ),
         desc.File(
             name='sensorDbPath',
-            label='Sensor Db Path',
+            label='Sensor Database',
             description='''Camera sensor width database path.''',
             value='${ALICEVISION_SENSOR_DB}',
-            uid=[0],
-        ),
-        desc.File(
-            name='voctreePath',
-            label='Voctree Path',
-            description='''Vocabulary tree path.''',
-            value='${ALICEVISION_VOCTREE}',
-            uid=[0],
-        ),
-        desc.BoolParam(
-            name='useSparseDistanceSelection',
-            label='Use Sparse Distance Selection',
-            description='Use sparseDistance selection in order to avoid similar keyframes.',
-            value=False,
-            uid=[0],
-        ),
-        desc.BoolParam(
-            name='useSharpnessSelection',
-            label='Use Sharpness Selection',
-            description='Use frame sharpness score for keyframe selection.',
-            value=False,
-            uid=[0],
-        ),
-        desc.FloatParam(
-            name='sparseDistMaxScore',
-            label='Sparse Distance Max Score',
-            description='Maximum number of strong common points between two keyframes.',
-            value=100.0,
-            range=(1.0, 200.0, 1.0),
-            uid=[0],
-        ),
-        desc.ChoiceParam(
-            name='sharpnessPreset',
-            label='Sharpness Preset',
-            description='Preset for sharpnessSelection : {ultra, high, normal, low, very_low, none}',
-            value='normal',
-            values=['ultra', 'high', 'normal', 'low', 'very_low', 'none'],
-            exclusive=True,
-            uid=[0],
-        ),
-        desc.IntParam(
-            name='sharpSubset',
-            label='Sharp Subset',
-            description='''sharp part of the image (1 = all, 2 = size/2, ...)''',
-            value=4,
-            range=(1, 100, 1),
-            uid=[0],
+            uid=[],
         ),
         desc.IntParam(
             name='minFrameStep',
@@ -153,20 +81,19 @@ You can extract frames at regular interval by configuring only the min/maxFrameS
             uid=[0],
         ),
         desc.IntParam(
-            name='maxFrameStep',
-            label='Max Frame Step',
-            description='''maximum number of frames after which a keyframe can be taken''',
-            value=2,
-            range=(2, 1000, 1),
-            uid=[0],
-        ),
-        desc.IntParam(
             name='maxNbOutFrame',
             label='Max Nb Out Frame',
             description='''maximum number of output frames (0 = no limit)''',
             value=0,
             range=(0, 10000, 1),
             uid=[0],
+        ),
+        desc.BoolParam(
+            name='useSimpleMethod',
+            label='Use simple method',
+            description="Disable smart algorithm for frame selection",
+            value=False,
+            uid=[],
         ),
         desc.ChoiceParam(
             name='verboseLevel',
@@ -188,4 +115,3 @@ You can extract frames at regular interval by configuring only the min/maxFrameS
             uid=[],
         ),
     ]
-

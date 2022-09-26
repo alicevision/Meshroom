06aa16243de31e420c337c177eb0c87ed6a48fa8
Fabien Servant <fabien.servant@technicolor.com>
2022-09-26 08:42:19 +0100

    [ION] wrong variable in script

--- bin/ion_start_meshroom.sh
+++ bin/ion_start_meshroom.sh
@@ -7,4 +7,4 @@ PATH_PLUGIN_TRUE=`readlink -f $PATH_PLUGIN`/../
 export QML2_IMPORT_PATH=$PATH_QML_TRUE:$QML2_IMPORT_PATH
 export QT_PLUGIN_PATH=$PATH_PLUGIN_TRUE:$QT_PLUGIN_PATH
 
-python $PATH_CONTAINER/base/meshroom/meshroom/ui
+python ${ION_CONTAINER_ROOT}/base/meshroom/meshroom/ui

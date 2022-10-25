4a231199cc8ec5b9fc49d9bf4a6345659cadcee0
Candice Bentéjac <candice.bentejac@gmail.com>
2022-10-25 11:14:56 +0200

    [qt6] Use exec() instead of deprecated exec_() in __main__.py
    In Python3, "exec" is not a reserved keyword anymore, and there is thus
    no need to distinguish the "exec" keyword from the PySide's function.

--- meshroom/ui/__main__.py
+++ meshroom/ui/__main__.py
@@ -8,4 +8,4 @@ meshroom.setupEnvironment(backend=Backend.PYSIDE)
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 from meshroom.ui.app import MeshroomApp
 app = MeshroomApp(sys.argv)
-app.exec_()
+app.exec()

2c216b9f085e2785233766dec9228629c030f5b4
Candice Bentéjac <candice.bentejac@gmail.com>
2022-12-05 17:10:09 +0100

    [docker] Remove Dockerfiles for Python 2

--- docker/extract.sh
+++ docker/extract.sh
@@ -8,17 +8,13 @@ test -z "$MESHROOM_VERSION" && MESHROOM_VERSION="$(git rev-parse --abbrev-ref HE
 test -z "$AV_VERSION" && echo "AliceVision version not specified, set AV_VERSION in the environment" && exit 1
 test -z "$CUDA_VERSION" && CUDA_VERSION="10.2"
 test -z "$CENTOS_VERSION" && CENTOS_VERSION="7"
-test -z "$MESHROOM_PYTHON2" || echo "========== Build for Python 2 =========="
-test -z "$MESHROOM_PYTHON2" || export PYTHON2_DOCKER_EXT="-py2"
-test -z "$MESHROOM_PYTHON2" || export PYTHON2_DOCKERFILE_EXT="_py2"
-test -z "$MESHROOM_PYTHON2" && echo "========== Build for Python 3 =========="
 
 test -d docker || (
 	echo This script must be run from the top level Meshroom directory
 	exit 1
 )
 
-VERSION_NAME=${MESHROOM_VERSION}-av${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION}${PYTHON2_DOCKER_EXT}
+VERSION_NAME=${MESHROOM_VERSION}-av${AV_VERSION}-centos${CENTOS_VERSION}-cuda${CUDA_VERSION}
 
 # Retrieve the Meshroom bundle folder
 rm -rf ./Meshroom-${VERSION_NAME}

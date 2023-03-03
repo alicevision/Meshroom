2fd080d8263b4c0d246ce04764ceb6ebd89ff0db
Fabien Servant <fabien.servant@technicolor.com>
2023-03-03 17:03:53 +0100

    [panorama] force pyramid levels count in compositing

--- meshroom/nodes/aliceVision/PanoramaCompositing.py
+++ meshroom/nodes/aliceVision/PanoramaCompositing.py
@@ -57,6 +57,15 @@ Multiple cameras are contributing to the low frequencies and only the best one c
             uid=[0]
         ),
         desc.IntParam(
+            name='forcedLevelsCount',
+            label='Pyramid levels',
+            description='Force the number of levels in the pyramid for multiband compositer.',
+            value=0,
+            range=(0, 64, 1),
+            uid=[],
+            enabled=lambda node: node.compositerType.value and node.compositerType.value == 'multiband',
+        ),
+        desc.IntParam(
             name='maxThreads',
             label='Max Nb Threads',
             description='Specifies the maximum number of threads to run simultaneously.',

b6d15b911f850c73047737c59b2fe1d8d7eb3968
Loïc Vital <mugulmotion@gmail.com>
2023-03-03 15:50:14 +0100

    [nodes] new ExportDistortion node

---
+++ meshroom/nodes/aliceVision/ExportDistortion.py
@@ -0,0 +1,31 @@
+__version__ = "1.0"
+
+from meshroom.core import desc
+
+class ExportDistortion(desc.AVCommandLineNode):
+    commandLine = 'aliceVision_exportDistortion {allParams}'
+
+    category = 'Export'
+    documentation = '''
+Export the distortion model and parameters of cameras in a SfM scene.
+'''
+
+    inputs = [
+        desc.File(
+            name='input',
+            label='Input SfMData',
+            description='SfMData file.',
+            value='',
+            uid=[0],
+        ),
+    ]
+
+    outputs = [
+        desc.File(
+            name='output',
+            label='Folder',
+            description='',
+            value=desc.Node.internalFolder,
+            uid=[],
+        ),
+    ]

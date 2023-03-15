53cc8898c14989595cc7733913606f5936db5325
Loïc Vital <mugulmotion@gmail.com>
2023-03-15 17:12:50 +0100

    [nodes] new ApplyCalibration node for aliceVision_applyCalibration software

---
+++ meshroom/nodes/aliceVision/ApplyCalibration.py
@@ -0,0 +1,38 @@
+__version__ = "1.0"
+
+from meshroom.core import desc
+
+class ApplyCalibration(desc.AVCommandLineNode):
+    commandLine = 'aliceVision_applyCalibration {allParams}'
+
+    category = 'Utils'
+    documentation = '''
+Overwrite intrinsics with a calibrated intrinsic.
+'''
+
+    inputs = [
+        desc.File(
+            name='input',
+            label='Input SfMData',
+            description='SfMData file.',
+            value='',
+            uid=[0],
+        ),
+        desc.File(
+            name='calibration',
+            label='Calibration',
+            description='Calibration SfMData file.',
+            value='',
+            uid=[0],
+        ),
+    ]
+
+    outputs = [
+        desc.File(
+            name='output',
+            label='SfMData File.',
+            description='Path to the output sfmData file.',
+            value=desc.Node.internalFolder + 'sfmData.sfm',
+            uid=[],
+        ),
+    ]

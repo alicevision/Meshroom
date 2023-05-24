962e0fa65f1e67e5f82bbc42a814203d58d3e44f
Candice Bentéjac <candice.bentejac@gmail.com>
2023-05-24 12:39:49 +0200

    [nodes] ExportDistortion: Add ST maps as viewable outputs

--- meshroom/nodes/aliceVision/ExportDistortion.py
+++ meshroom/nodes/aliceVision/ExportDistortion.py
@@ -28,4 +28,22 @@ Export the distortion model and parameters of cameras in a SfM scene.
             value=desc.Node.internalFolder,
             uid=[],
         ),
+        desc.File(
+            name='distoStMap',
+            label='Distortion ST Map',
+            description='Calibrated distortion ST map.',
+            semantic='image',
+            value=desc.Node.internalFolder + '<INTRINSIC_ID>_distort.exr',
+            group='',  # do not export on the command line
+            uid=[],
+        ),
+        desc.File(
+            name='undistoStMap',
+            label='Undistortion ST Map',
+            description='Calibrated undistortion ST map.',
+            semantic='image',
+            value=desc.Node.internalFolder + '<INTRINSIC_ID>_undistort.exr',
+            group='',  # do not export on the command line
+            uid=[],
+        ),
     ]

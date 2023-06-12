2fc9bd63c11f83bc7f6dbed89e7c14eeb5d827c8
jmelou <jean.melou@gmail.com>
2023-06-12 10:23:38 +0200

    [PS] Change node category to PhotometricStereo

--- meshroom/nodes/aliceVision/PhotometricStereo.py
+++ meshroom/nodes/aliceVision/PhotometricStereo.py
@@ -4,7 +4,7 @@ from meshroom.core import desc
 
 class PhotometricStereo(desc.CommandLineNode):
     commandLine = 'aliceVision_photometricStereo {allParams}'
-    category = 'Photometry'
+    category = 'PhotometricStereo'
     documentation = '''
 Reconstruction using Photometric Stereo. A normal map is evaluated from several photographs taken from the same point of view, but under different lighting conditions.
 The lighting conditions are assumed to be known.

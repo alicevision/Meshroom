1bb156828041c24307325d3327017b17b8f9a349
Loïc Vital <mugulmotion@gmail.com>
2023-06-21 18:24:58 +0200

    [nodes] ImageSegmentation: set GPU level for farm submission

--- meshroom/nodes/aliceVision/ImageSegmentation.py
+++ meshroom/nodes/aliceVision/ImageSegmentation.py
@@ -6,6 +6,7 @@ from meshroom.core import desc
 class ImageSegmentation(desc.AVCommandLineNode):
     commandLine = 'aliceVision_imageSegmentation {allParams}'
     size = desc.DynamicNodeSize('input')
+    gpu = desc.Level.NORMAL
 
     category = 'Utils'
     documentation = '''

d18cadc5ec4afd2126e272002789b12c604f5b60
Fabien Servant <fabien.servant@technicolor.com>
2023-10-05 11:15:56 +0200

    Update chunks with merge

--- meshroom/nodes/aliceVision/SfMMerge.py
+++ meshroom/nodes/aliceVision/SfMMerge.py
@@ -3,9 +3,35 @@ __version__ = "1.0"
 from meshroom.core import desc
 import os.path
 
+
+class MergeNodeSize(object):
+    """
+    MergeNodeSize expresses a dependency to two input attributess to define
+    the size of a Node in terms of individual tasks for parallelization.
+    """
+    def __init__(self, param1, param2):
+        self._param1 = param1
+        self._param2 = param2
+
+    def computeSize(self, node):
+        
+        size1 = 0
+        size2 = 0
+
+        param1 = node.attribute(self._param1)
+        if param1.isLink:
+            size1 = param1.getLinkParam().node.size
+
+        param2 = node.attribute(self._param2)
+        if param2.isLink:
+            size2 = param2.getLinkParam().node.size
+        
+        return size1 + size2
+
+
 class SfmMerge(desc.AVCommandLineNode):
     commandLine = 'aliceVision_sfmMerge {allParams}'
-    size = desc.DynamicNodeSize('firstinput')
+    size = MergeNodeSize('firstinput', 'secondinput')
 
     category = 'Utils'
     documentation = '''

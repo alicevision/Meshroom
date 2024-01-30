406e518e0315d5f7665c7ace00b83ee350f18dd5
Candice Bentéjac <candice.bentejac@gmail.com>
2024-01-30 13:46:39 +0100

    [nodes] ImportE57: Clean-up labels and descriptions

--- meshroom/nodes/aliceVision/ImportE57.py
+++ meshroom/nodes/aliceVision/ImportE57.py
@@ -4,26 +4,34 @@ from meshroom.core import desc
 
 
 class ImportE57(desc.AVCommandLineNode):
-    commandLine = 'aliceVision_importe57 {allParams}'
+    commandLine = 'aliceVision_importE57 {allParams}'
     size = desc.DynamicNodeSize('input')
 
     category = 'Utils'
     documentation = '''
-Import an e57 file and generate a sfmData
+Import an E57 file and generate an SfMData.
 '''
 
     inputs = [
         desc.ListAttribute(
             elementDesc=desc.File(
                 name="inputFile",
-                label="e57 File",
-                description="Path to a e57 file.",
+                label="E57 File",
+                description="Path to an E57 file.",
                 value="",
                 uid=[0],
             ),
             name="input",
             label="Input Files",
-            description="Set of e57 files in the same reference frame."
+            description="Set of E57 files in the same reference frame."
+        ),
+        desc.FloatParam(
+            name="maxDensity",
+            label="Points Density",
+            description="Ensure no points has no neighbour closer than maxDensity meters.",
+            value=0.01,
+            range=(0.0, 0.2, 0.001),
+            uid=[0]
         ),
         desc.ChoiceParam(
             name="verboseLevel",
@@ -33,15 +41,6 @@ Import an e57 file and generate a sfmData
             values=["fatal", "error", "warning", "info", "debug", "trace"],
             exclusive=True,
             uid=[],
-        ),
-
-        desc.FloatParam(
-            name="maxDensity",
-            label="Points density",
-            description="Make sure no points has no neighboor closer than maxDensity meters",
-            value=0.01,
-            range=(0.0, 0.2, 0.001),
-            uid=[0]
         )
     ]
 
@@ -54,4 +53,3 @@ Import an e57 file and generate a sfmData
             uid=[],
         ),
     ]
-

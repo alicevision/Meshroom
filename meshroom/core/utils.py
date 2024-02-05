b8d14fe6be3ff6ef66ac5342bd921d3a6faf5ad5
Candice Bentéjac <candice.bentejac@gmail.com>
2024-02-05 11:13:54 +0000

    [core] `utils.py`: Add new utilitary constants for `ChoiceParams`
    In addition to COLORSPACES, add VERBOSE_LEVEL, DESCRIBER_TYPES,
    EXR_STORAGE_DATA_TYPE and RAW_COLOR_INTERPRETATION which are used
    across several nodes.

--- meshroom/core/utils.py
+++ meshroom/core/utils.py
@@ -3,3 +3,13 @@ COLORSPACES = ["AUTO", "sRGB", "rec709", "Linear", "ACES2065-1", "ACEScg", "Line
                "BMDFilm WideGamut Gen5", "CanonLog2 CinemaGamut D55", "CanonLog3 CinemaGamut D55",
                "Linear CinemaGamut D55", "Linear V-Gamut", "V-Log V-Gamut", "Linear REDWideGamutRGB",
                "Log3G10 REDWideGamutRGB", "Linear Venice S-Gamut3.Cine", "S-Log3 Venice S-Gamut3.Cine", "no_conversion"]
+
+DESCRIBER_TYPES = ["sift", "sift_float", "sift_upright", "dspsift", "akaze", "akaze_liop", "akaze_mldb", "cctag3",
+                   "cctag4", "sift_ocv", "akaze_ocv", "tag16h5", "unknown"]
+
+EXR_STORAGE_DATA_TYPE = ["float", "half", "halfFinite", "auto"]
+
+RAW_COLOR_INTERPRETATION = ["None", "LibRawNoWhiteBalancing", "LibRawWhiteBalancing", "DCPLinearProcessing",
+                            "DCPMetadata", "Auto"]
+
+VERBOSE_LEVEL = ["fatal", "error", "warning", "info", "debug", "trace"]

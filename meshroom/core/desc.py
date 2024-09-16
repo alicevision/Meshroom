99dc6b5c6a4823971374fc80eed02acaad8d424e
Candice Bentéjac <candice.bentejac@gmail.com>
2024-09-16 18:03:34 +0200

    [core] Make `range` optional for `IntParam` and `FloatParam`
    `range` is set to `None` by default, making it optional. If it is
    specified, correct values are expected (e.g. only floats for `FloatParam`)
     and a slider will be displayed in the Node Editor. However, if it is not
    specified, no slider will be displayed and the value will be set at the
    discretion of the user.
    
    Having `range=None` will not trigger any "invalid" error in the
    description. It is thus not mandatory anymore to specify the valid range
    of values for the integer and floating attributes in node descriptions.

--- meshroom/core/desc.py
+++ meshroom/core/desc.py
@@ -336,7 +336,7 @@ class BoolParam(Param):
 class IntParam(Param):
     """
     """
-    def __init__(self, name, label, description, value, range, group="allParams", advanced=False, enabled=True,
+    def __init__(self, name, label, description, value, range=None, group="allParams", advanced=False, enabled=True,
                  invalidate=True, semantic="", validValue=True, errorMessage="", visible=True, exposed=False):
         self._range = range
         super(IntParam, self).__init__(name=name, label=label, description=description, value=value,
@@ -366,7 +366,7 @@ class IntParam(Param):
 class FloatParam(Param):
     """
     """
-    def __init__(self, name, label, description, value, range, group="allParams", advanced=False, enabled=True,
+    def __init__(self, name, label, description, value, range=None, group="allParams", advanced=False, enabled=True,
                  invalidate=True, semantic="", validValue=True, errorMessage="", visible=True, exposed=False):
         self._range = range
         super(FloatParam, self).__init__(name=name, label=label, description=description, value=value,

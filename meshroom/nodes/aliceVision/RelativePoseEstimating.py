616d7598a000ec999be9498b79088889b8f65cef
Fabien Servant <fabien.servant@technicolor.com>
2025-01-14 10:48:35 +0100

    add minInliers parameter (optional)

--- meshroom/nodes/aliceVision/RelativePoseEstimating.py
+++ meshroom/nodes/aliceVision/RelativePoseEstimating.py
@@ -42,6 +42,14 @@ Estimate relative pose between each pair of views that share tracks.
             range=(1024, 500000, 1),
             advanced=True,
         ),
+        desc.IntParam(
+            name="minInliers",
+            label="Ransac min inliers",
+            description="Minimal allowed inliers in two view relationship.",
+            value=35,
+            range=(1, 1000, 1),
+            advanced=True,
+        ),
         desc.ChoiceParam(
             name="verboseLevel",
             label="Verbose Level",

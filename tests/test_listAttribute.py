4642f3c9727a1121b95ef4ae63fff0d3227f2a18
Yann Lanthony <yann.lanthony@gmail.com>
2025-01-29 10:24:57 +0100

    [core] ListAttribute: fix methods not considering connected value
    Fix `__len__`, `__iter__`, `at` and `index` to consider linked ListAttribute
    value when it is connected.

---
+++ tests/test_listAttribute.py
@@ -0,0 +1,63 @@
+from meshroom.core import desc
+from meshroom.core.graph import Graph
+from meshroom.core import registerNodeType, unregisterNodeType
+
+
+class NodeWithListAttribute(desc.Node):
+    inputs = [
+        desc.ListAttribute(
+            name="listInput",
+            label="List Input",
+            description="ListAttribute of StringParams.",
+            elementDesc=desc.StringParam(name="value", label="Value", description="", value=""),
+        )
+    ]
+
+
+class TestListAttribute:
+
+    @classmethod
+    def setup_class(cls):
+        registerNodeType(NodeWithListAttribute)
+
+    @classmethod
+    def teardown_class(cls):
+        unregisterNodeType(NodeWithListAttribute)
+
+    def test_lengthUsesLinkParam(self):
+        graph = Graph("")
+
+        nodeA = graph.addNewNode(NodeWithListAttribute.__name__)
+        nodeB = graph.addNewNode(NodeWithListAttribute.__name__)
+
+        graph.addEdge(nodeA.listInput, nodeB.listInput) 
+
+        nodeA.listInput.append("test")
+
+        assert len(nodeB.listInput) == 1
+
+    def test_iterationUsesLinkParam(self):
+        graph = Graph("")
+
+        nodeA = graph.addNewNode(NodeWithListAttribute.__name__)
+        nodeB = graph.addNewNode(NodeWithListAttribute.__name__)
+
+        graph.addEdge(nodeA.listInput, nodeB.listInput) 
+
+        nodeA.listInput.extend(["A", "B", "C"])
+
+        for value in nodeB.listInput:
+            assert value.node == nodeA
+
+    def test_elementAccessUsesLinkParam(self):
+        graph = Graph("")
+
+        nodeA = graph.addNewNode(NodeWithListAttribute.__name__)
+        nodeB = graph.addNewNode(NodeWithListAttribute.__name__)
+
+        graph.addEdge(nodeA.listInput, nodeB.listInput) 
+
+        nodeA.listInput.extend(["A", "B", "C"])
+
+        assert nodeB.listInput.at(0).node == nodeA
+        assert nodeB.listInput.index(nodeB.listInput.at(0)) == 0

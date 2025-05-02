97196d99411d103551b810a14ca07b7e024d4cd8
nicolas-lambert-tc <nicolas.lambert@technicolor.com>
2025-05-02 11:33:03 +0200

    [ui] Attribute: Add tests to check retrieving the linked input/output attributes

---
+++ tests/test_attributes.py
@@ -0,0 +1,29 @@
+from meshroom.core.graph import Graph
+
+
+def test_attribute_retrieve_linked_input_and_output_attributes():
+    """
+    Check that an attribute can retrieve the linked input and output attributes
+    """
+
+    # n0 -- n1 -- n2
+    #   \          \
+    #    ---------- n3
+
+    g = Graph('')
+    n0 = g.addNewNode('Ls', input='/tmp')
+    n1 = g.addNewNode('Ls', input=n0.output)
+    n2 = g.addNewNode('Ls', input=n1.output)
+    n3 = g.addNewNode('AppendFiles', input=n1.output, input2=n2.output)
+
+    # check that the attribute can retrieve its linked input attributes
+    assert len(n0.input.getInputAttributes()) == 0
+    assert len(n1.input.getInputAttributes()) == 1
+    assert n1.input.getInputAttributes()[0] == n0.output
+    
+    assert len(n1.output.getOutputAttributes()) == 2
+    
+    assert n1.output.getOutputAttributes()[0] == n2.input
+    assert n1.output.getOutputAttributes()[1] == n3.input
+
+    

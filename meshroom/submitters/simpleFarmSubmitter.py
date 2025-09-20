33bc3ae3a37ae7ca07d5584a28734f5f63240d58
Fabien Castan <fabcastan@gmail.com>
2025-09-20 22:34:03 +0200

    [submitter] Propagate some env variables

--- meshroom/submitters/simpleFarmSubmitter.py
+++ meshroom/submitters/simpleFarmSubmitter.py
@@ -63,6 +63,12 @@ class SimpleFarmSubmitter(BaseSubmitter):
         if 'REZ_PROD_PACKAGES_PATH' in os.environ:
             self.environment['REZ_PROD_PACKAGES_PATH'] = os.environ['REZ_PROD_PACKAGES_PATH']
 
+        if 'PROD' in os.environ:
+            self.environment['PROD'] = os.environ['PROD']
+
+        if 'PROD_ROOT' in os.environ:
+            self.environment['PROD_ROOT'] = os.environ['PROD_ROOT']
+
     def createTask(self, meshroomFile, node):
         tags = self.DEFAULT_TAGS.copy()  # copy to not modify default tags
         nbFrames = node.size

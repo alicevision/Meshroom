51cfd91bdb4715d30a7bd4a513879fcf6ab54302
Alice Sonolet <alice.sonolet@gmail.com>
2025-10-21 11:41:27 +0200

    [submitters] Remove submitters from meshroom to develop them in mrSubmitters

--- meshroom/core/submitter.py
+++ meshroom/core/submitter.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 from meshroom.common import BaseObject, Property
+import logging
+
+logger = logging.getLogger("Submitter")
+logger.setLevel(logging.INFO)
 
 
 class BaseSubmitter(BaseObject):
     def __init__(self, name, parent=None):
         super().__init__(parent)
         self._name = name
+        logger.info(f"Registered submitter {self._name}")
 
     def submit(self, nodes, edges, filepath, submitLabel="{projectName}"):
         """ Submit the given graph

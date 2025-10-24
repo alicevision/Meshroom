4b48098f5dfa58d8caecf315ce062bb7a128eb72
Candice Bentéjac <candice.bentejac@gmail.com>
2025-10-24 18:30:23 +0200

    Update version to 2025.1.1

--- meshroom/__init__.py
+++ meshroom/__init__.py
@@ -9,9 +9,9 @@ class VersionStatus(Enum):
     develop = 2
 
 
-__version__ = "2026.1.0"
+__version__ = "2025.1.1"
 # Always increase the minor version when switching from release to develop.
-__version_status__ = VersionStatus.develop
+__version_status__ = VersionStatus.release
 
 if __version_status__ is VersionStatus.develop:
     __version__ += "+" + __version_status__.name
--- tests/plugins/meshroom/sharedTemplate.mg
+++ tests/plugins/meshroom/sharedTemplate.mg
@@ -1,6 +1,6 @@
 {
     "header": {
-        "releaseVersion": "2025.1.0-develop",
+        "releaseVersion": "2025.1.1",
         "fileVersion": "2.0",
         "nodesVersions": {},
         "template": true

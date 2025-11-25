b92fe86673bcc8fa34088ddcfd2cfb03d9f3154f
Candice Bentéjac <candice.bentejac@gmail.com>
2025-11-25 13:41:48 +0100

    [ui] Remove unused imports

--- meshroom/ui/app.py
+++ meshroom/ui/app.py
@@ -7,7 +7,7 @@ from enum import Enum
 
 from PySide6 import __version__ as PySideVersion
 from PySide6 import QtCore
-from PySide6.QtCore import QObject, QUrl, QJsonValue, qInstallMessageHandler, QtMsgType, QSettings
+from PySide6.QtCore import QUrl, QJsonValue, qInstallMessageHandler, QtMsgType, QSettings
 from PySide6.QtGui import QIcon
 from PySide6.QtQml import QQmlDebuggingEnabler
 from PySide6.QtQuickControls2 import QQuickStyle

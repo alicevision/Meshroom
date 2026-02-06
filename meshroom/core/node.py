eef8a217de4eca95418843eeacbf2ac85d3e0b11
Candice Bentéjac <candice.bentejac@gmail.com>
2026-02-06 17:32:11 +0100

    [core] node: Check whether a node's an `InputNode` with its property

--- meshroom/core/node.py
+++ meshroom/core/node.py
@@ -1798,14 +1798,14 @@ class BaseNode(BaseObject):
         Returns:
             Status: the node global status
         """
-        if isinstance(self.nodeDesc, desc.InputNode):
+        if self.isInputNode:
             return Status.INPUT
         if not self._chunksCreated:
             # Get status from nodeStatus
             return self._nodeStatus.status
         if not self._chunks:
             return Status.NONE
-        if len( self._chunks) == 1:
+        if len(self._chunks) == 1:
             return self._chunks[0]._status.status
 
         chunksStatus = [chunk._status.status for chunk in self._chunks]
@@ -2266,7 +2266,7 @@ class Node(BaseNode):
         """ Set chunks on the node.
         # TODO : Maybe don't delete chunks if we will recreate them as before ?
         """
-        if isinstance(self.nodeDesc, desc.InputNode):
+        if self.isInputNode:
             self._chunksCreated = True
             return
         # Disconnect signals
@@ -2354,7 +2354,7 @@ class Node(BaseNode):
         """ Create chunks when computation is about to start. """
         if self._chunksCreated:
             return
-        if isinstance(self.nodeDesc, desc.InputNode):
+        if self.isInputNode:
             self._chunksCreated = True
             self.chunksChanged.emit()
             return

014e210c8407c20ae0a8f98290c4744e80432f1e
Fabien Castan <fabcastan@gmail.com>
2026-03-02 20:03:10 +0100

    [tests] CommandLineFormatting: Add unit test using a function

--- tests/test_nodeCommandLineFormatting.py
+++ tests/test_nodeCommandLineFormatting.py
@@ -31,6 +31,35 @@ class NodeWithCommandLineFormatting_usingNodeAndLambda(desc.CommandLineNode):
         ),
     ]
 
+def customFunction_commandline(node):
+    return f"myapp --input {node.input.value} --output {node.output.value}"
+
+
+class NodeWithCommandLineFormatting_usingNodeAndFunction(desc.CommandLineNode):
+    """
+    A node using a function for the commandLine member variable.
+    """
+    commandLine = customFunction_commandline
+
+    inputs = [
+        desc.File(
+            name="input",
+            label="Input File",
+            description="An input file.",
+            value="/some/input",
+        ),
+    ]
+
+    outputs = [
+        desc.File(
+            name="output",
+            label="Output",
+            description="Output file.",
+            value="output.txt",
+        ),
+    ]
+
+
 class NodeWithCommandLineFormatting_usingNode(desc.CommandLineNode):
     """
     A node using a lambda for the commandLine member variable.
@@ -86,34 +115,45 @@ class TestCommandLineFormatting:
     @classmethod
     def setup_class(cls):
         registerNodeDesc(NodeWithCommandLineFormatting_usingNodeAndLambda)
+        registerNodeDesc(NodeWithCommandLineFormatting_usingNodeAndFunction)
         registerNodeDesc(NodeWithCommandLineFormatting_usingNode)
         registerNodeDesc(NodeWithCommandLineFormatting_usingValue)
 
     @classmethod
     def teardown_class(cls):
         unregisterNodeDesc(NodeWithCommandLineFormatting_usingNodeAndLambda)
+        unregisterNodeDesc(NodeWithCommandLineFormatting_usingNodeAndFunction)
         unregisterNodeDesc(NodeWithCommandLineFormatting_usingNode)
         unregisterNodeDesc(NodeWithCommandLineFormatting_usingValue)
 
     def test_commandLine_node(self):
         graph = Graph("")
         nodeNL = graph.addNewNode("NodeWithCommandLineFormatting_usingNodeAndLambda")
+        nodeNF = graph.addNewNode("NodeWithCommandLineFormatting_usingNodeAndFunction")
         nodeN = graph.addNewNode("NodeWithCommandLineFormatting_usingNode")
         nodeV = graph.addNewNode("NodeWithCommandLineFormatting_usingValue")
 
         nodeNL.input.value = "/path/in"
+        nodeNF.input.value = "/path/in"
         nodeN.input.value = "/path/in"
         nodeV.input.value = "/path/in"
+
         nodeNL._buildExpVars()  # populate _expVars
+        nodeNF._buildExpVars()  # populate _expVars
         nodeN._buildExpVars()  # populate _expVars
         nodeV._buildExpVars()  # populate _expVars
 
         cmdNL = nodeNL.nodeDesc.buildCommandLine(nodeNL.chunks[0])
+        cmdNF = nodeNL.nodeDesc.buildCommandLine(nodeNF.chunks[0])
         cmdN = nodeN.nodeDesc.buildCommandLine(nodeN.chunks[0])
         cmdV = nodeV.nodeDesc.buildCommandLine(nodeV.chunks[0])
+
         assert cmdNL
+        assert cmdNF
         assert cmdN
         assert cmdV
-        assert cmdNL == cmdN
+
+        assert cmdNL == cmdNF
+        assert cmdN == cmdNL
         assert cmdN == cmdV
 

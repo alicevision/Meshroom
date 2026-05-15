fa3ff9ae9fa0c878c8b3ba4faa940543ee670a9f
Candice Bentéjac <candice.bentejac@gmail.com>
2026-05-15 16:34:54 +0200

    [tests] Clean-up: Linting on test node `NodeWithValidators`

--- tests/nodes/test/nodeValidators.py
+++ tests/nodes/test/nodeValidators.py
@@ -6,27 +6,27 @@ class NodeWithValidators(desc.CommandLineNode):
 
     inputs = [
         desc.StringParam(
-            name='mandatory',
-            label='Mandatory input',
-            description='''''',
-            value='',
+            name="mandatory",
+            label="Mandatory Input",
+            description="",
+            value="",
             validators= [
                 NotEmptyValidator()
             ]
         ),
         desc.FloatParam(
-            name='floatRange',
-            label='range input',
-            description='''''',
+            name="floatRange",
+            label="Range Input",
+            description="",
             value=0.0,
             validators=[
                 RangeValidator(min=0.0, max=1.0)
             ]
         ),
         desc.IntParam(
-            name='intRange',
-            label='range input',
-            description='''''',
+            name="intRange",
+            label="Range Input",
+            description="",
             value=0,
             validators=[lambda node, attr: success() if 0 <= attr.value < 5 else error("Value should be in range 0-5")]
         ),
@@ -35,10 +35,9 @@ class NodeWithValidators(desc.CommandLineNode):
 
     outputs = [
         desc.File(
-            name='output',
-            label='Output',
-            description='''''',
-            value='{nodeCacheFolder}/appendText.txt',
+            name="output",
+            label="Output",
+            description="",
+            value="{nodeCacheFolder}/appendText.txt",
         )
     ]
-

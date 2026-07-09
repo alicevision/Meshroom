3aca07bbe7379e070db148918b15a81fa7519457
Alice Sonolet <alice.sonolet@gmail.com>
2026-07-09 18:19:14 +0200

    GraphInput: enhance code

--- meshroom/nodes/general/GraphInput.py
+++ meshroom/nodes/general/GraphInput.py
@@ -18,10 +18,12 @@ class GraphInput(desc.InputNode, desc.InitNode):
 
     def getParams(self, node):
         serializedAnySet = node.inputs.getSerializedValue()
-        serializedChildren = serializedAnySet.get("children")
+        serializedChildren = serializedAnySet.get("children") or []
         parameters = [p["name"] for p in serializedChildren]
         return parameters
 
     def setInputAttribute(self, node, attrName, attrValue):
         attr = node.getAnyAttribute(f"inputs.{attrName}")
+        if not attr:
+            raise KeyError(f"No GraphInput attribute named {attrName}")
         attr._setValue(attrValue)

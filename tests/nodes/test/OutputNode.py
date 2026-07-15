10424abf9d3d8046665e7d6ec51ec17c026b7018
nicolas-lambert-tc <nicolas.lambert@technicolor.com>
2026-07-15 18:11:20 +0200

    [tests] Add OutputNode unit tests

---
+++ tests/nodes/test/OutputNode.py
@@ -0,0 +1,41 @@
+from typing import ClassVar
+
+from meshroom.core import desc
+
+
+class OutputNodeTest(desc.Node, desc.OutputNode):
+    outputAttributes: ClassVar[list[str]] = ["folder", "outputFile", "exportLabel", "exportEnabled"]
+
+    inputs = [
+        desc.File(
+            name="folder",
+            label="Folder",
+            description="Output folder.",
+            value="/default/output",
+        ),
+        desc.File(
+            name="outputFile",
+            label="File",
+            description="Secondary output path.",
+            value="",
+        ),
+        desc.StringParam(
+            name="exportLabel",
+            label="Label",
+            description="Export label.",
+            value="default",
+        ),
+        desc.BoolParam(
+            name="exportEnabled",
+            label="Enabled",
+            description="Export enabled.",
+            value=True,
+        ),
+        desc.StringParam(
+            name="internalLabel",
+            label="Internal Label",
+            description="Internal label.",
+            value="internal",
+        ),
+    ]
+    outputs = []
